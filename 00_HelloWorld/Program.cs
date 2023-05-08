5fc4f46beecd973f63ce757aac9ea6f94d58470f
Brianne W <brianne4god@gmail.com>
2023-05-08 14:26:44 -0400

    Created first C# Project

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//console.log("Hello, World!");
+
+// C# is a compiled language
+// JavaScript is interpreted
+
+// Before running C#, we must build and compile it
+//dotnet build to build the project
+// dotnet run -- project 00_HelloWorld (be sepecific)
+
+// dotnet watch provides a hot reload which will run the aplication after the changes are saved
+
+//ctrl+c to stop dotnet watch
+
+Console.WriteLine("My name is Brianne!");
