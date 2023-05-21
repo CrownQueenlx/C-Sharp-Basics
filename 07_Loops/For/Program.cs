7e22772aa71c0f8e9bd50449f145cc254af2ddaa
Brianne W <brianne4god@gmail.com>
2023-05-21 10:01:02 -0400

    For Loops and While Loops

---
+++ 07_Loops/For/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Creating an intitial target
+int number = 100;
+
+// 1    //2       //3        //4
+for (int i = 0 ; i < number; i++)
+{
+    // 5
+    Console.WriteLine(i);
+}
+
+// 1 - for keyword
+// 2 - Declaration and initialization of an iterator variable
+// 3 - Boolean expression comparing the new interatior to the number varible
+// 4 - Incrementing the iterator at the end of each loop
+// 5 - Body of the loop, runs once per iteration after the boolean is evaluated
+
+Console.Write("Input a number to count to from zero "); 
+string Input = Console.ReadLine() ?? "0";
+int baseNum = int.Parse(Input);
+baseNum = Convert.ToInt32(Input);
+
+int numberInput = baseNum;
+
+// 1    //2       //3        //4
+for (int i = 0 ; i <= numberInput; i++)
+{
+    // 5
+    Console.WriteLine(i);
+}
---
+++ 07_Loops/While/Program.cs
@@ -0,0 +1,10 @@
+﻿// Declating a variable to use in out loop's boolean expression
+int number = 0;
+
+// While the value in number is less than or equal to 100, run the following
+while (number <= 100)
+{
+    // Write the value of number to the console and then increament by 5
+    Console.WriteLine(number);
+    number += 5;
+}
